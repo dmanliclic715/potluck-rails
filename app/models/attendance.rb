f285b67072308c6a99843db18adbbc38093aad8c
dmanliclic715 <dmanliclic715@gmail.com>
2016-10-28 22:33:37 -0500

    Add seed data to database

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,5 @@
 class Attendance < ApplicationRecord
-  belongs_to :guest, class_name: "User", foreign_key :user_id
+  belongs_to :guest, class_name: "User", foreign_key: :user_id
   belongs_to :potluck
 
   validates :dish, presence: true, uniqueness: { case_sensitive: false, scope: :potluck_id, message: "is already being brought" }
