7eef5dd5a7b2c09f737bc801e6056fa93b702f4d
dmanliclic715 <dmanliclic715@gmail.com>
2016-10-31 19:35:52 -0500

    Edit sessions controller and allow for log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to root_path
     else
+      puts "!!!!!!!!! in the else"
       render 'new'
     end
   end
-  
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+
 end
